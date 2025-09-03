4f8bb40c3593111e3e5db20a25d55add02312931
Joao Vera <joaoguedessoaresvera@gmail.com>
2025-09-03 14:38:39 +0200

    file setup

--- simple_process_ex/simple_process_ex.cpp
+++ simple_process_ex/simple_process_ex.cpp
@@ -7,15 +7,28 @@ void simple_process_ex::my_thread_process(void) {
 
 void simple_process_ex::clock(void) {
   while (true) {
-    wait(0.5, SC_SEC);
     clk.write(SC_LOGIC_0);
-    cout << "Clock tick at: " << sc_time_stamp() << std::endl;
-    wait(0.5, SC_SEC);
+    wait(5, SC_NS);
     clk.write(SC_LOGIC_1);
+    wait(5, SC_NS);
   }
 }
 
 void simple_process_ex::counter(void) {
   count.write(count.read() + 1);
-  std::cout << "Counter value: " << count.read() << std::endl;
+}
+
+void simple_process_ex::sender(void) {
+  while (true)
+  {
+    fifo.write(count.read());
+    wait(10, SC_NS);
+  }
+}
+
+void simple_process_ex::printer(void) {
+  while (true)
+  {
+      std::cout << "Counter value: " << fifo.read() << " at simulation time: " << sc_time_stamp() <<std::endl;
+  }
 }
--- simple_process_ex/simple_process_ex.h
+++ simple_process_ex/simple_process_ex.h
@@ -4,18 +4,26 @@
 
 SC_MODULE(simple_process_ex) {
   
+  // Ports and signals
+  sc_fifo<int> fifo;
+  sc_signal<sc_logic> clk;
+  sc_signal<int> count;
+  
   SC_CTOR(simple_process_ex) {
     SC_THREAD(my_thread_process);
     SC_THREAD(clock);
     SC_METHOD(counter);
       sensitive << clk.posedge_event();
+      dont_initialize();
+    SC_THREAD(sender);
+    SC_THREAD(printer);
   }
 
-  sc_signal<sc_logic> clk;
-  sc_signal<int> count;
-
+  // Process declarations
   void my_thread_process(void);
   void clock(void);
   void counter(void);
+  void sender(void);
+  void printer(void);
 };
 #endif
